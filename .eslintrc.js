68322e8fe412a45a03b63e2ff808ffba9d16d33d
thienc45 <doanducthien123@gmail>
2024-03-14 22:08:27 +0700

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Các cài đặt khác của ESLint
+    rules: {
+      'eslint-disable-next-line': 'off', // Tắt cảnh báo về việc sử dụng eslint-disable-next-line
+      // Các quy tắc khác nếu cần
+    },
+  };
+  
