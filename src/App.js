f946c755a958455c0d04cbe3b3a5155a9587cd48
thienc45 <doanducthien123@gmail>
2024-03-15 09:33:23 +0700

    .

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ const App = () => {
   const [mapCenter, setMapCenter] = useState({ lat: 37.7749, lng: -122.4194 });
 
   useEffect(() => {
-    fetch("http://localhost:8081/api/food-trucks")
+    // fetch("http://localhost:8081/api/food-trucks")
+    fetch("http://54.251.142.187:8081/api/food-trucks")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -35,7 +36,7 @@ const App = () => {
 
   const handleSearch = () => {
     fetch(
-      `http://localhost:8081/api/food-trucks/search?locationDescription=${searchQuery}`
+      `http://54.251.142.187:8081/api/food-trucks/search?locationDescription=${searchQuery}`
     )
       .then((response) => {
         if (!response.ok) {
